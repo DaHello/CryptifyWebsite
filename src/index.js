b019540444708adb11d4c948db8c567dfa3285a2
CAMIl0-git <118149460+AR-CA-SA@users.noreply.github.com>
2024-11-07 19:11:36 -0500

    explanation of render

--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+//in the hmtl, the div with the id "root" is the containter that will be used
+//to render html contents
+///I'm assuming that you can also do that with entire files, like in the code above, but as 
+//we learn react we will figure things out.
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<p>HELLO, this is a test</p>)
+
+//change the code aboce if you want to try it out and get an idea of how it works
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
