6b23ca54062d1b69a337356db441c9f82f74be91
Robert <robertkane650@outlook.com>
2024-11-12 14:21:44 -0500

    11/12

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import LoginPage from "./Components/LoginPage/Login"
+import LoginPage from './components/LoginPage/Login';
+import './styles/App.css';
 
 function App() {
   return (
@@ -9,3 +10,37 @@ function App() {
 }
 
 export default LoginPage;
+
+// old code (keep for reference):
+/*
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          This is a Test Line of code by me
+          This will appear in the GitHub Desktop app as a modified file from
+          the respository on GitHub.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
