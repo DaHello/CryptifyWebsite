7bb48794a1eca3e06f4cfa3daff36f58ab554da5
CAMIl0-git <118149460+AR-CA-SA@users.noreply.github.com>
2024-11-16 11:03:38 -0500

    16/2024 ref

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
 import LoginPage from './components/Login';
 import Dashboard from './components/Dashboard';
 
 export default function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<LoginPage />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-            </Routes>
-        </Router>
+        // <Router>
+        //     <Routes>
+        //         <Route path="/" element={<LoginPage />} />
+        //         <Route path="/dashboard" element={<Dashboard />} />
+        //     </Routes>
+        // </Router>
+        <h1>hi</h1>
     );
 }
