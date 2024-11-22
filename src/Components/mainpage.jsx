da066ab3cc62579318240dcbb6af813229f7a3da
Robert <robertkane650@outlook.com>
2024-11-22 00:33:22 -0500

    11/22

--- src/Components/mainpage.jsx
+++ src/Components/mainpage.jsx
@@ -2,6 +2,9 @@ import { DecForm } from "./mainpagecomponents/decStruct";
 import { EncForm } from "./mainpagecomponents/encStruct";
 import "../styles/mainPage.css";
 
+//function genForm() {}
+
+// main page is where all of the forms are going to be loaded
 export function MainPage() {
   return (
     <div className="container">
