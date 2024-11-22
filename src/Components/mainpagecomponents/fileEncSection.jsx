542ac5cd36dcfce88aef786a69dfb8fe09ed67dc
CAMIl0-git <118149460+AR-CA-SA@users.noreply.github.com>
2024-11-22 03:45:03 -0500

    drop zone test-do not change without permission 11/22

--- src/Components/mainpagecomponents/fileEncSection.jsx
+++ src/Components/mainpagecomponents/fileEncSection.jsx
@@ -1,12 +1,13 @@
 import openpgp from "openpgp";
 import { useDropzone } from "react-dropzone";
 
-
+import { DropZoneArea } from "./dropZone";
 
 export const EncFileBox = () =>{
     return(
         <div className="fileHandlerBox">
             <input type="file"></input>
+            <DropZoneArea></DropZoneArea>
         </div>
     );
 }
