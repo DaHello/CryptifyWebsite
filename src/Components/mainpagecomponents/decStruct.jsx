60a7b7df987fe40f086e1ed413609f1c252707a2
CAMIl0-git <118149460+AR-CA-SA@users.noreply.github.com>
2024-11-22 00:32:33 -0500

    encdec 11/22

--- src/Components/mainpagecomponents/decStruct.jsx
+++ src/Components/mainpagecomponents/decStruct.jsx
@@ -1,23 +1,108 @@
 import { DecSubmit } from "./decButton";
 import "../../styles/mainPage.css";
 
+import React, { useState } from "react";
+import { FaPassport } from "react-icons/fa6";
+
+
+
 export const DecForm = () =>{
-    return (
-        <form className="genForm">
-            <label hmtlfor = "non-defined1">Place holder for Decryption</label>
-     
-            <textarea id = "non-defined1" type="text" className="ioBox"></textarea>
+  const [textDec, setTextDec] = useState("");
+  const [keyDec, setKeyDec] = useState("");
+  const [outputDec, setOutPutDec] = useState("");
+  const enc = new TextEncoder();
 
-            <label htmlfor ="key">key</label>
+  const dec = new TextDecoder();
 
-            <input id = "key" type="textarea"  className="keyBox"></input>
+  const base64_to_buf = (b64) =>
+    Uint8Array.from(atob(b64), (c) => c.charCodeAt(0));
 
-            <label hmtlfor = "maybe-output1"  >Place holder for output</label>
 
-            <textarea id = "maybe-output1" type="text" className="ioBox"></textarea>
+    const getPasswordKey = (password) =>
+      window.crypto.subtle.importKey(
+        "raw",
+        enc.encode(password),
+        "PBKDF2",
+        false,
+        ["deriveKey"]
+      );
 
-            <DecSubmit></DecSubmit>
-        </form>
-   );
+    const deriveKey = (passwordKey, salt, keyUsage) =>
+      window.crypto.subtle.deriveKey(
+        {
+          name: "PBKDF2",
+          salt: salt,
+          iterations: 250000,
+          hash: "SHA-256",
+        },
+        passwordKey,
+        { name: "AES-GCM", length: 256 },
+        false,
+        keyUsage
+      );
+  
 
+  async function decryptData(encryptedData, password) {
+    try {
+      const encryptedDataBuff = base64_to_buf(encryptedData);
+      const salt = encryptedDataBuff.slice(0, 16);
+      const iv = encryptedDataBuff.slice(16, 16 + 12);
+      const data = encryptedDataBuff.slice(16 + 12);
+      const passwordKey = await getPasswordKey(password);
+      const aesKey = await deriveKey(passwordKey, salt, ["decrypt"]);
+      const decryptedContent = await window.crypto.subtle.decrypt(
+        {
+          name: "AES-GCM",
+          iv: iv,
+        },
+        aesKey,
+        data
+      );
+      return dec.decode(decryptedContent);
+    } catch (e) {
+      console.log(`Error - ${e}`);
+      return "";
+    }
+  }
+  async function handleSubmitDec(e) {
+    e.preventDefault()
+    const dectext_key = { textDec, keyDec };
+    const decrypted = await decryptData(dectext_key.textDec, dectext_key.keyDec)
+    if(dectext_key.keyDec !=)
+    setOutPutDec(decrypted)
+
+    console.log(decrypted)
+  }
+  return (
+    <form className="genForm" onSubmit={handleSubmitDec}>
+      <label>Place holder for Decryption</label>
+
+      <textarea
+        type="text"
+        className="ioBox"
+        value={textDec}
+        onChange={(e) => setTextDec(e.target.value)}
+      ></textarea>
+
+      <label>key</label>
+
+      <input
+        type="textarea"
+        className="keyBox"
+        value={keyDec}
+        onChange={(e) => setKeyDec(e.target.value)}
+      ></input>
+
+      <label>Place holder for output</label>
+
+      <textarea
+        type="text"
+        className="ioBox"
+        value={outputDec}
+        readOnly
+      ></textarea>
+
+      <DecSubmit></DecSubmit>
+    </form>
+  );
 }
