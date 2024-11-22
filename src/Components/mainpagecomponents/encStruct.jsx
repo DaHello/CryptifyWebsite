1fe5d7c61f0e029542166e2ceb775da33534322d
Robert <robertkane650@outlook.com>
2024-11-21 21:14:36 -0500

    11/21

--- src/Components/mainpagecomponents/encStruct.jsx
+++ src/Components/mainpagecomponents/encStruct.jsx
@@ -2,52 +2,145 @@ import { EncSubmit } from "./encButton";
 import "../../styles/mainPage.css";
 
 import React, { useState } from "react";
-
 import { getKey } from "./edcLogic.js";
 
+export const EncForm = () => {
+  const [text, setText] = useState("");
+  const [key, setKey] = useState("");
+  const [outPutT, setOPT] = useState(""); // this can be used to show the encrypted/decrypted output
 
-export const EncForm = () =>{
-//text, default state | setText, "new" state
-    const [text, setText] = useState('');
-    const [key, setKey] = useState('');
-    const [outPutT, setOPT] = useState('');
-    
+  const base64_to_buf = (b64) =>
+    Uint8Array.from(atob(b64), (c) => c.charCodeAt(0));
 
-    function handleSubmit(e){
-        //doesnt allow the page to reset
-        e.preventDefault();
-        
-
-        
-        const text_key = {text,key};
-        //get data from json to encrypt
-        getKey(text_key.key, text_key.text)
-
-
-        
-
-
-    }
-    //onChange(the value you want to change)=>setText(current value. target is the current input, value is the current value in that input)
-
-    return (
-        <form className="genForm" onSubmit={handleSubmit}>
-            <label hmtlfor = "non-defined2">Place holder for Encryption</label>
- 
-            <textarea id = "non-defined2" type="text"  className="ioBox" required value={text} onChange={(text)=>setText(text.target.value)}></textarea>
-            
-            <label htmlfor ="key">key</label>
-
-            <input id = "key" type="text"  className="keyBox" required  value={key} onChange={(key)=>setKey(key.target.value)}></input>
-
-            <label hmtlfor = "maybe-output2"  >Place holder for output</label>
-
-            <textarea type="text" id ="maybe-output2" className="ioBox"></textarea>
-
-            <EncSubmit></EncSubmit>
-            {/* <p>{text}</p> */}
-            <p>{key}</p>
-        </form>
+  const buff_to_base64 = (buff) =>
+    btoa(
+      new Uint8Array(buff).reduce(
+        (data, byte) => data + String.fromCharCode(byte),
+        ""
+      )
     );
 
-}
+  const enc = new TextEncoder();
+  const dec = new TextDecoder();
+
+  async function encryptData(secretData, password) {
+    try {
+      const salt = window.crypto.getRandomValues(new Uint8Array(16));
+      const iv = window.crypto.getRandomValues(new Uint8Array(12));
+      const passwordKey = await getPasswordKey(password);
+      const aesKey = await deriveKey(passwordKey, salt, ["encrypt"]);
+      const encryptedContent = await window.crypto.subtle.encrypt(
+        {
+          name: "AES-GCM",
+          iv: iv,
+        },
+        aesKey,
+        enc.encode(secretData)
+      );
+
+      const encryptedContentArr = new Uint8Array(encryptedContent);
+      let buff = new Uint8Array(
+        salt.byteLength + iv.byteLength + encryptedContentArr.byteLength
+      );
+      buff.set(salt, 0);
+      buff.set(iv, salt.byteLength);
+      buff.set(encryptedContentArr, salt.byteLength + iv.byteLength);
+      const base64Buff = buff_to_base64(buff);
+      return base64Buff;
+    } catch (e) {
+      console.log(`Error - ${e}`);
+      return "";
+    }
+  }
+
+  async function decryptData(encryptedData, password) {
+    try {
+      const encryptedDataBuff = base64_to_buf(encryptedData);
+      const salt = encryptedDataBuff.slice(0, 16);
+      const iv = encryptedDataBuff.slice(16, 16 + 12);
+      const data = encryptedDataBuff.slice(16 + 12);
+      const passwordKey = await getPasswordKey(password);
+      const aesKey = await deriveKey(passwordKey, salt, ["decrypt"]);
+      const decryptedContent = await window.crypto.subtle.decrypt(
+        {
+          name: "AES-GCM",
+          iv: iv,
+        },
+        aesKey,
+        data
+      );
+      return dec.decode(decryptedContent);
+    } catch (e) {
+      console.log(`Error - ${e}`);
+      return "";
+    }
+  }
+
+  const getPasswordKey = (password) =>
+    window.crypto.subtle.importKey(
+      "raw",
+      enc.encode(password),
+      "PBKDF2",
+      false,
+      ["deriveKey"]
+    );
+
+  const deriveKey = (passwordKey, salt, keyUsage) =>
+    window.crypto.subtle.deriveKey(
+      {
+        name: "PBKDF2",
+        salt: salt,
+        iterations: 250000,
+        hash: "SHA-256",
+      },
+      passwordKey,
+      { name: "AES-GCM", length: 256 },
+      false,
+      keyUsage
+    );
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const text_key = { text, key };
+    const encrypted = await encryptData(text_key.text, text_key.key);
+    setOPT(encrypted); // Set the encrypted output to state
+
+    // Optionally, you could decrypt right after encryption to verify
+    const decrypted = await decryptData(encrypted, text_key.key);
+    console.log("Decrypted Text: ", decrypted);
+  }
+
+  return (
+    <form className="genForm" onSubmit={handleSubmit}>
+      <label htmlFor="non-defined2">Place holder for Encryption</label>
+      <textarea
+        placeholder="type encrypted text here..."
+        type="text"
+        className="ioBox"
+        required
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
+
+      <label htmlFor="key">key</label>
+      <input
+        type="text"
+        className="keyBox"
+        required
+        value={key}
+        onChange={(e) => setKey(e.target.value)}
+      ></input>
+
+      <label htmlFor="maybe-output2">Encrypted Output</label>
+      <textarea
+        type="text"
+        className="ioBox"
+        value={outPutT}
+        readOnly
+      ></textarea>
+
+      <EncSubmit />
+    </form>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';  // this is new path after putting CSS files into "styles" folder
-
-import App from './App';  // component from App.js, function called App()
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css"; // this is new path after putting CSS files into "styles" folder
+import App from "./App"; // component from App.js, function called App()
+import reportWebVitals from "./reportWebVitals";
 
 // have html, where all pages start.
 // so we should not have any html files
@@ -19,15 +18,14 @@ import reportWebVitals from './reportWebVitals';
 
 //in the hmtl, the div with the id "root" is the containter that will be used
 //to render html contents
-///I'm assuming that you can also do that with entire files, like in the code above, but as 
+///I'm assuming that you can also do that with entire files, like in the code above, but as
 //we learn react we will figure things out.
-
 
 /*
 This code brings all the pieces together and injects the final product 
 into index.html in the public folder
 */
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
@@ -35,7 +33,6 @@ root.render(
 );
 
 //change the code aboce if you want to try it out and get an idea of how it works
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
