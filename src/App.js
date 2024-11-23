aaa1456d2906257c135f16552aa016c92a337a43
Robert <robertkane650@outlook.com>
2024-11-22 19:26:25 -0500

    from master

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
 // src/App.js
 import React, { useState } from "react";
-//import NavBar from "./components/NavBar"; // navigation bar
 //import Home from "./components/Home"; // about us and other info
-import { createBrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { createBrowserRouter, Route, creat } from "react-router-dom";
 //import LoginPage from "./components/Login";
 //import Dashboard from "./components/Dashboard";
 import { MainPage } from "./components/mainpage";
 import "./styles/Login.css";
-// import {MainPage} from "./components/mainpage";
-// import "./styles/App.css";
-
-/* 
-this is where all react components that are web pages should be located 
-each web page can have data sent from one to the other using {object/variable name}
-inside of the <MainPage {var/object} /> for example.
-*/
-//export default function App() {
-//   const [activeTab, setActiveTab] = useState("home");
-
-//   const renderTabContent = () => {
-//     switch (activeTab) {
-//       case "home":
-//         return <Home />;
-//       case "encryptFiles":
-//         return <EncryptFiles />;
-//       case "encryptText":
-//         return <EncryptText />;
-//       default:
-//         return <Home />;
-//     }
-//  };
 
 //this is the header
 // const router = createBrowserRouter(
@@ -44,6 +20,17 @@ inside of the <MainPage {var/object} /> for example.
 
 //import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
+//tree of routes
+const router = createBrowserRouter(
+  //similar to <routes>?
+  createRoutesFromElements(
+    <Route path="mainpagetext" exact element={<MainPages />}>
+      <Route path="mainpagetext" element={<MainPageText />}></Route>
+      <Route path="mainpagefile" element={<MainPageFile />}></Route>
+    </Route>
+  )
+);
+
 export default function App() {
   // Load the header of the App (website), inject code for header
 
@@ -53,36 +40,10 @@ export default function App() {
   // Have the aboutUs.jsx
 
   return (
-    <>
-      <MainPage />
+    <RouterProvider router={router} />
 
-      {/* <rt>
-          <Router>
-            <div className="App">
-              <Link to="/mainpage">MainPage</Link>
-              <Link to="/home">Home</Link>
-
-              <Routes>
-                <Route path="/mainpage" element={<MainPage />} />
-                <Route path="/home" element={<Home />} />
-              </Routes>
-            </div>
-          </Router>
-        </rt> */}
-    </>
+    // <>
+    //   <MainPage />
+    // </>
   );
 }
-
-// old code...
-// return (
-//   <>
-//     <MainPage />
-//   </>
-
-// <Router>
-//     <Routes>
-//         <Route path="/" element={<LoginPage />} />
-//         <Route path="/dashboard" element={<Dashboard />} />
-//     </Routes>
-// </Router>
-// );
